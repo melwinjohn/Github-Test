c692c904d4c38d1c62336f3fd23cb355b77a372b
Melwin John <43521501+melwinjohn@users.noreply.github.com>
2025-10-27 23:27:03 -0700

    Test feature (#1)
    * Add multiply and subtract functions
    
    * Update package.json with new scripts and version bump
    
    * Fix: Remove unused import from feature.js
    
    ---------
    
    Co-authored-by: Melwin Samuel John <mjohn@personifycorp.com>

---
+++ feature.js
@@ -0,0 +1,15 @@
+// Feature implementation
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log("Feature: Multiply 4 * 5 =", multiply(4, 5));
+console.log("Feature: Subtract 10 - 3 =", subtract(10, 3));
+
+module.exports = { multiply, subtract };
+
